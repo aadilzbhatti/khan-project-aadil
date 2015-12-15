68c6697af5294d07a29e5162ea2635194f2920ed
prevosis <aadilzbhatti@gmail.com>
2015-12-15 00:12:55 -0600

    commenting

--- total_infection.py
+++ total_infection.py
@@ -2,7 +2,10 @@ from nodes import Coach
 
 """
 The total infection algorithm. Infects entire classroom in a 
-depth-first traversal of the class. 
+depth-first traversal of the class. If the original argument
+has a coach, then we recursively infect the parent coach so 
+as to infect both the current coach (and potential students)
+as well as the parent coach's students.
 """
 def total_infection(coach):
 	if isinstance(coach, Student) or coach.coach is not None:
