594af2cf87546fd74d88826da2660c0b5d6d12fd
prevosis <aadilzbhatti@gmail.com>
2015-12-14 23:43:53 -0600

    renamed for simplicity

---
+++ total_infection.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from nodes import Coach
+
+"""
+The total infection algorithm. Infects entire classroom in a 
+depth-first traversal of the class. 
+"""
+def total_infection(coach):
+	coach.infect()
+	s = [coach]
+	while s:
+		v = s.pop()
+		v.infect()
+		if isinstance(v, Coach):
+			for student in v.students:
+				s.append(student)
